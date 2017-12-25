1e77b2f5525fb113b2ef6fd5311807b2da98bc78
Q <shi.qiu.1222@gmail.com>
2017-12-26 03:05:58 +0800

    4
    4

--- cpp_LeetCode_algo_3.cpp
+++ cpp_LeetCode_algo_3.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 int SubstringStartWithIdx(string& s, int idx);
 int lengthOfLongestSubstring(string s);
 
+
+
+
+
 int main(){
-    string txt = "abcabcdebb";
-//    int ans = 0;
-//    for(int i=0;i<(int)txt.size();i++){
-//        ans = max(SubstringStartWithIdx(txt, i), ans);
-//    }
-//    cout <<ans << endl;
+    string txt = "abcabcbb";
+//    string txt = "pwwkew";
+//    string txt = "abba";
+//    string txt = "tmmzuxt";
 
     cout << lengthOfLongestSubstring(txt) << endl;
 
@@ -20,49 +23,51 @@ int main(){
 }
 
 
-//int SubstringStartWithIdx(string& s, int idx) {
+int lengthOfLongestSubstring(string s) {
+    if(s.empty()) return 0;
+
+    unordered_map<char, int> charIdx;
+    int startIdx = 0;
+    int ans = 1;
+    for(int i=0;i<s.size();i++){
+        if(!charIdx.count(s[i]) || charIdx[s[i]]<startIdx){
+            ans = max(ans, i-startIdx+1);
+        }
+        else{
+            startIdx = charIdx[s[i]]+1;
+        }
+        charIdx[s[i]] = i;
+    }
+
+    return ans;
+}
+
+
+///*
+//sliding window solution:
+//拿俩index i和j
+//j为前部index
+//i为后部index
+//先查看是否s[j]已经出现过
+//没出现过: j++, length = max(ori_length, j-i)
+//出现过: 不再改变j，说明此时以s[i]打头的substring已经不好使了，相反的，我们应该换一个新的打头的，即i++
+//        同时删除出现过的记录字符
+//*/
+//int lengthOfLongestSubstring(string s) {
+//    int n = s.length();
 //    unordered_set<char> charSet;
-//    int ans = 0;
-//    for(int i=idx; i<(int)s.size(); i++){
-//        if(charSet.count(s[i])){
-//            return ans;
+//    int ans = 0, i = 0, j = 0;
+//    while (i < n && j < n) {
+//        // try to extend the range [i, j]
+//        if (!charSet.count(s[j])){
+//            charSet.insert(s[j]);
+//            j++;
+//            ans = max(ans, j - i);
 //        }
-//        else{
-//            ans++;
-//            charSet.insert(s[i]);
+//        else {
+//            charSet.erase(s[i]);
+//            i++;
 //        }
 //    }
 //    return ans;
 //}
-
-
-
-/*
-sliding window solution:
-这个algo是真牛B.
-拿俩index i和j
-j为前部index
-i为后部index
-先查看是否s[j]已经出现过
-没出现过: j++, length = max(ori_length, j-i)
-出现过: 不再改变j，说明此时以s[i]打头的substring已经不好使了，相反的，我们应该换一个新的打头的，即i++
-        同时删除出现过的记录字符
-*/
-int lengthOfLongestSubstring(string s) {
-    int n = s.length();
-    unordered_set<char> charSet;
-    int ans = 0, i = 0, j = 0;
-    while (i < n && j < n) {
-        // try to extend the range [i, j]
-        if (!charSet.count(s[j])){
-            charSet.insert(s[j]);
-            j++;
-            ans = max(ans, j - i);
-        }
-        else {
-            charSet.erase(s[i]);
-            i++;
-        }
-    }
-    return ans;
-}
--- cpp_LeetCode_algo_4.cpp
+++ cpp_LeetCode_algo_4.cpp
@@ -2,56 +2,171 @@
 #include <vector>
 
 using namespace std;
-void MergeSort(vector<int>& v1, vector<int>& v2, vector<int>& ans);
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) ;
+
+//double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//    int size1 = nums1.size();
+//    int size2 = nums2.size();
+//    int halfSize = (size1+size2+1)/2;
+//
+//    // pick first n1 elements from nums1, first n2 elements from nums2, which sum = halfSize
+//    // suppose size1 < size2
+//    if(size1>size2) return findMedianSortedArrays(nums2, nums1);
+//
+//    int lidx = 0; // left split pos, partition 0th~(lsp-1)th, (lsp)th~end
+//    int ridx = nums1.size()-1; // right split pos, similar as above
+//
+//    while(lidx <= ridx){
+//        int mid = lidx+(ridx-lidx)/2;
+//        int n1 = mid-lidx+1;
+//        int n2 = halfSize - n1;
+//
+//        if(nums1[n1-1] > nums2[n2]){// picked too much elements from nums1
+//            ridx = mid-1;
+//        }
+//        else if(nums2[n2-1] > nums1[n1]){ // picked too much elements from nums2
+//            lidx = mid+1;
+//        }
+//        else{ // found the correct boundary
+//            if( (size1+size2) % 2 == 1){
+//                return (double)max(nums1[n1-1], nums2[n2-1]);
+//            }
+//            else{
+//                double a = (double)max(nums1[n1-1], nums2[n2-1]);
+//                double b = (double)min(nums1[n1], nums2[n2]);
+//                return (a+b)*0.5;
+//            }
+//        }
+//    }
+//
+//    // nums1 doesn't provide any elements, it has nothing to do with nums1
+//    if(ridx<=0){
+//        if( (size1+size2) % 2 == 1){
+//            return (double)nums2[halfSize-1];
+//        }
+//        else{
+//            return (nums2[halfSize-1]+nums2[halfSize])*0.5;
+//        }
+//    }
+//
+//    // nums1 are all picked
+//    if(lidx>=size1){
+//        if(halfSize-size1==0){
+//            if((size1+size2) % 2 == 1){
+//                return (double)nums1[size1-1];
+//            }
+//            else{
+//                return (nums1[size1-1]+nums2[0])*0.5;
+//            }
+//        }
+//        else{
+//            if((size1+size2) % 2 == 1){
+//                return (double)nums2[halfSize-size1-1];
+//            }
+//            else{
+//                return (nums2[halfSize-size1-1]+nums2[halfSize-size1-2])*0.5;
+//            }
+//        }
+//    }
+//
+////    // nums1 provides all elements, it has nothing to do with nums2
+////    if(size1==size2){
+////        return (nums1[size1-1]+nums2[0])*0.5;
+////    }
+//
+//
+//}
+
+
+
+
+
 
 int main(){
-    vector<int> v1 = {1,3};
-    vector<int> v2 = {2};
-    vector<int> ans;
-    MergeSort(v1,v2,ans);
+    vector<int> v1 = {3};
+    vector<int> v2 = {1,2};
 
-    for(auto each:ans){
-        cout << each << " ";
-    }
-    cout << endl;
-
-    int size = ans.size();
-    if(size % 2){
-        cout << "odd " << (double)ans[size/2] << endl;
-    }
-    else{
-        cout << "even " << (double)(((double)ans[size/2] + (double)ans[size/2-1])/2) << endl;
-    }
+    cout.precision(6);
+    cout <<findMedianSortedArrays(v1,v2)<< endl;
 
     return 0;
 }
 
 
-void MergeSort(vector<int>& v1, vector<int>& v2, vector<int>& ans){
-    int idx1=0, idx2=0;
-    int size1 = v1.size();
-    int size2 = v2.size();
 
-    while(idx1!=size1 || idx2!=size2){
-        if(v1[idx1] <= v2[idx2]){
-            ans.push_back(v1[idx1]);
-            idx1++;
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    int size1 = nums1.size();
+    int size2 = nums2.size();
+    int halfSize = (size1+size2+1)/2;
+
+    // pick first n1 elements from nums1, first n2 elements from nums2, which sum = halfSize
+    // suppose size1 < size2
+    if(size1>size2) return findMedianSortedArrays(nums2, nums1);
+
+    int lsp = 0;
+    int rsp = size1;
+    int midsp;
+    int nElem1 = 0;
+    int nElem2 = 0;
+
+    while(lsp < rsp){
+        midsp = (rsp+lsp+1)/2;
+        nElem1 = midsp-lsp;
+        nElem2 = halfSize-nElem1;
+
+        if(nums1[nElem1-1] > nums2[nElem2] && nElem2<size2){ // picked too much from nums1
+            rsp = midsp-1;
+        }
+        else if(nums2[nElem2-1] > nums1[nElem1] && nElem1<size1){ // pick too less from nums1
+            lsp = midsp;
         }
         else{
-            ans.push_back(v2[idx2]);
-            idx2++;
+            break;
         }
     }
 
-    if(idx1==size1){
-        vector<int>::iterator it = v2.begin();
-        advance(it, idx2);
-        ans.insert(ans.end(), it, v2.end());
+
+    // if reach here, we have 2 possibilities:
+    // 1. all elements in nums1 are picked -> lsp = size1
+    // 2. no elements in nums1 is picked -> rsp = 0
+    // 3. found elements needed in nums1 and nums2
+
+    if(nElem1 = size1){ //all elements in nums1 are picked
+        if(halfSize-size1 == 0){ // no elements need from nums2
+            if((size1+size2) % 2==1){
+                return nums1[size1-1];
+            }
+            else{
+//                double a = nums1[size1-1] + nums2[0];
+//                a *= 0.5;
+                return (nums1[size1-1] + nums2[0])*0.5;
+            }
+        }
+        else{ // need some elements from nums2
+            if((size1+size2) % 2==1){
+                return nums2[halfSize-size1-1];
+            }
+            else{
+                return (nums2[halfSize-size1-1] + nums2[halfSize-size1])*0.5;
+            }
+        }
     }
 
-    if(idx2==size2){
-        vector<int>::iterator it = v1.begin();
-        advance(it, idx1);
-        ans.insert(ans.end(), it, v1.end());
+    if(nElem1==0){ // no elements in nums1 is picked, all should be from nums2
+        if((size1+size2) % 2==1){
+            return nums2[halfSize-1];
+        }
+        else{
+            return (nums2[halfSize-1] + nums2[halfSize])*0.5;
+        }
+    }
+
+
+    if( (size1+size2) % 2==1 ){
+        return max(nums1[nElem1-1], nums2[nElem2-1]);
+    }
+    else{
+        return (max(nums1[nElem1-1], nums2[nElem2-1])+min(nums1[nElem1], nums2[nElem2]))*0.5;
     }
 }
