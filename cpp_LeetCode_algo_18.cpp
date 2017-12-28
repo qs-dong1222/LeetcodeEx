ecb22453e66fb2ec861f882bd765250b00539893
Q <shi.qiu.1222@gmail.com>
2017-12-28 20:01:32 +0800

    18
    18

--- cpp_LeetCode_algo_18.cpp
+++ cpp_LeetCode_algo_18.cpp
@@ -24,44 +24,43 @@ int main(){
 }
 
 
-vector<vector<int>> fourSum(vector<int>& nums, int target) {
+
+
+
+
+
+vector<vector<int>> fourSum(vector<int>& nums, int target){
     vector<vector<int>> ans;
-    if(nums.empty()){
-        return ans;
-    }
+    if(nums.empty() || nums.size()<4) return ans;
 
     sort(nums.begin(), nums.end());
 
-    vector<int> tmp;
-    int target_2sum;
-    int lidx, ridx;
+    for(int i=0;i<(int)nums.size()-3;i++){
+        for(int j=i+1;j<(int)nums.size()-2;j++){
+            int l = j+1;
+            int r = nums.size()-1;
 
-    for(int i=0; i<nums.size()-3; i++){
-        for(int j=i+1; j<nums.size()-2; j++){
-            target_2sum = target - nums[i] - nums[j];
-            lidx = j+1;
-            ridx = nums.size()-1;
-
-            while(lidx < ridx){
-                if(nums[lidx] + nums[ridx] == target_2sum){
-                    tmp = {nums[i], nums[j], nums[lidx], nums[ridx]};
-                    ans.push_back(tmp);
-                    lidx++;
-                    while(lidx < ridx && nums[lidx] == nums[lidx-1]) lidx++;
-                    while(lidx < ridx && nums[ridx] == nums[ridx+1]) ridx--;
+            while(l < r){
+                int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                if(sum>target){
+                    r--;
                 }
-                else if(nums[lidx] + nums[ridx] < target_2sum)
-                    lidx++;
-                else
-                    ridx--;
+                else if(sum<target){
+                    l++;
+                }
+                else{
+                    vector<int> curr = {nums[i], nums[j], nums[l], nums[r]};
+                    ans.push_back(curr);
+                    // skip repetitive nums
+                    while(l<r && nums[l+1]==nums[l]) l++;
+                    while(l<r && nums[r-1]==nums[r]) r--;
+                    l++;
+                    r--;
+                }
             }
-
-            while(j<nums.size()-2 && nums[j+1] == nums[j])
-                j++;
+            while(j<(int)(nums.size()-2) && nums[j+1]==nums[j]) j++; // skip repetitive nums
         }
-
-        while(i<nums.size()-3 && nums[i+1] == nums[i])
-            i++;
+        while(i<(int)(nums.size()-3) && nums[i+1]==nums[i]) i++; // skip repetitive nums
     }
 
     return ans;
@@ -69,12 +68,60 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
 
 
 
-//void changeMinIdx(vector<int>& nums, int& idx1, int& idx2, int& idx3){
-//    int idxA, idxB, idxC;
-//    idxA = nums[idx1] - nums[idx1-1] <= nums[idx2] - nums[idx2-1] ? idx1 : idx2;
-//    idxB = nums[idx2] - nums[idx2-1] <= nums[idx3] - nums[idx3-1] ? idx2 : idx3;
-//    idxC = nums[idxA] - nums[idxA-1] <= nums[idxB] - nums[idxB-1] ? idxA : idxB;
-//    if(idxC == idx1) idx1--;
-//    if(idxC == idx2) idx2--;
-//    if(idxC == idx3) idx3--;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//vector<vector<int>> fourSum(vector<int>& nums, int target) {
+//    vector<vector<int>> ans;
+//    if(nums.empty()){
+//        return ans;
+//    }
+//
+//    sort(nums.begin(), nums.end());
+//
+//    vector<int> tmp;
+//    int target_2sum;
+//    int lidx, ridx;
+//
+//    for(int i=0; i<nums.size()-3; i++){
+//        for(int j=i+1; j<nums.size()-2; j++){
+//            target_2sum = target - nums[i] - nums[j];
+//            lidx = j+1;
+//            ridx = nums.size()-1;
+//
+//            while(lidx < ridx){
+//                if(nums[lidx] + nums[ridx] == target_2sum){
+//                    tmp = {nums[i], nums[j], nums[lidx], nums[ridx]};
+//                    ans.push_back(tmp);
+//                    lidx++;
+//                    while(lidx < ridx && nums[lidx] == nums[lidx-1]) lidx++;
+//                    while(lidx < ridx && nums[ridx] == nums[ridx+1]) ridx--;
+//                }
+//                else if(nums[lidx] + nums[ridx] < target_2sum)
+//                    lidx++;
+//                else
+//                    ridx--;
+//            }
+//
+//            while(j<nums.size()-2 && nums[j+1] == nums[j])
+//                j++;
+//        }
+//
+//        while(i<nums.size()-3 && nums[i+1] == nums[i])
+//            i++;
+//    }
+//
+//    return ans;
 //}
+
