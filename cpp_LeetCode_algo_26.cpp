211eaa251765bc5370c8573115bee2e66c5fdd58
Q <shi.qiu.1222@gmail.com>
2017-12-29 20:35:31 +0800

    26
    26

--- cpp_LeetCode_algo_26.cpp
+++ cpp_LeetCode_algo_26.cpp
@@ -6,30 +6,26 @@ using namespace std;
 int removeDuplicates(vector<int>& nums);
 
 int main(){
-    vector<int> v = {0,0,0,0,0};
-    sort(v.begin(), v.end());
+    vector<int> v = {2,3,3,3,4,4,5,6};
 
     cout << removeDuplicates(v) << endl;
 
     for(auto each: v){
-        cout << each << endl;
+        cout << each << " ";
     }
 
     return 0;
 }
 
-
-
 int removeDuplicates(vector<int>& nums) {
-    vector<int>::iterator it=nums.begin();
-    int i;
-    for(i=0; i<(int)nums.size()-1; i++){
-        while(nums[i]==nums[i+1]){
-            nums.erase(i+it);
-            if(nums.size() == 1) break;
-            if(i==(int)nums.size()-1) break;
+    if((int)nums.size()<=1) return nums.size();
+
+    for(int i=1;i<(int)nums.size();i++){
+        if(nums[i]==nums[i-1]){
+            nums.erase(nums.begin()+i);
+            i--;
         }
     }
-    if(nums[nums.size()-1] == nums[nums.size()-2]) nums.erase(nums.size()-1+it);
+
     return nums.size();
 }
