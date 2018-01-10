b2f88f43b8434669aecaab8b96455abe3eaf25cf
Q <shi.qiu.1222@gmail.com>
2018-01-11 06:08:01 +0800

    55
    55

--- cpp_LeetCode_algo_55.cpp
+++ cpp_LeetCode_algo_55.cpp
@@ -8,10 +8,47 @@ bool canJump(vector<int>& nums);
 int main(){
     vector<int> nums = {3,2,1,0,4};
 
-    canJump(nums);
+    cout << canJump(nums) << endl;
 
     return 0;
 }
+
+
+
+bool canJump(vector<int>& nums) {
+    if(nums.empty()) return true;
+
+    int start = 0;
+    int end = 0;
+    int nextEnd = end;
+
+    while(start <= end){
+        for(; start<=end; start++){
+            nextEnd = max(start+nums[start], nextEnd);
+        }
+        if(nextEnd >= (int)nums.size()-1){
+            return true;
+        }
+        else{
+            start = end+1;
+            end = nextEnd;
+        }
+    }
+
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
@@ -19,26 +56,26 @@ int main(){
 反之，如果当前范围找不到更远的范围，就说明当前范围只能在当前范围里跑，就卡在当前范围了。
 那也就不能跳到最后，那么就是失败。反之，一直前进就有可能成功。
 */
-bool canJump(vector<int>& nums) {
-    if(nums.empty()) return false;
-
-    int startIdx = 0;
-    int endIdx = 0;
-    int tempIdx;
-    int maxEnd = endIdx;
-    while(endIdx < nums.size()-1){
-        for(tempIdx = startIdx; tempIdx<=endIdx; tempIdx++){
-            //找下一个range的最远endIdx
-            maxEnd = max(maxEnd, nums[tempIdx]+tempIdx);
-            //如果下一个范围能跑的超过lastidx，说明能跑到最后
-            if(maxEnd >= nums.size()-1) return true;
-        }
-
-        startIdx = endIdx+1;
-        //如果当前范围找到的下一个endidx还在当前范围内，说明就卡在当前范围了，不能成功
-        if(maxEnd <= endIdx) return false;
-        endIdx = maxEnd;
-    }
-    //只有当endIdx跑到最后了，才有可能跳出循环，那就说明能成功
-    return true;
-}
+//bool canJump(vector<int>& nums) {
+//    if(nums.empty()) return false;
+//
+//    int startIdx = 0;
+//    int endIdx = 0;
+//    int tempIdx;
+//    int maxEnd = endIdx;
+//    while(endIdx < nums.size()-1){
+//        for(tempIdx = startIdx; tempIdx<=endIdx; tempIdx++){
+//            //找下一个range的最远endIdx
+//            maxEnd = max(maxEnd, nums[tempIdx]+tempIdx);
+//            //如果下一个范围能跑的超过lastidx，说明能跑到最后
+//            if(maxEnd >= nums.size()-1) return true;
+//        }
+//
+//        startIdx = endIdx+1;
+//        //如果当前范围找到的下一个endidx还在当前范围内，说明就卡在当前范围了，不能成功
+//        if(maxEnd <= endIdx) return false;
+//        endIdx = maxEnd;
+//    }
+//    //只有当endIdx跑到最后了，才有可能跳出循环，那就说明能成功
+//    return true;
+//}
